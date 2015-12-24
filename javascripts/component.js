2d6355a23b228a46217e8af3e48d8e80e26d27f7
Kelvin <kelvindk@yahoo.com>
2015-12-24 19:12:41 +0800

    drag & drop

--- javascripts/component.js
+++ javascripts/component.js
@@ -1,24 +1,38 @@
 
 function Component(name, pageX, pageY, dimX, dimY) {
 	this.name = name;
+	this.color = "rgba(255, 255, 255, 0.8)";
 	this.pageX = pageX;
 	this.pageY = pageY;
 	this.dimX = dimX;
 	this.dimY = dimY;
+	this.boundaryX = pageX + dimX;
+	this.boundaryY = pageY + dimY;
+}
+
+Component.prototype.update = function(pageX, pageY) {
+	this.pageX = pageX;
+	this.pageY = pageY;
 
 }
 
-Component.prototype.alt = function()
-{
+Component.prototype.alt = function() {
 	alert(this.name+" "+this.pageX+" "+this.pageY+" "+this.labelX()+" "+this.labelY());
 }
 
-Component.prototype.labelX = function()
-{
-	return this.pageX+this.dimX/4;
+Component.prototype.labelX = function() {
+	return this.pageX+this.dimX/10;
 }
 
-Component.prototype.labelY = function()
-{
-	return this.pageY+this.dimY/2;
+Component.prototype.labelY = function() {
+	return this.pageY+this.dimY/4;
+}
+
+Component.prototype.isComponentClicked = function(pageX, pageY, layoutScale) {
+	
+	var clicked = (pageX > this.pageX*layoutScale)&&(pageY > this.pageY*layoutScale)&&
+		(pageX < this.boundaryX*layoutScale)&&(pageY < this.boundaryY*layoutScale) ? true: false;
+	// var rtn = pageX+" "+pageY+" "+clicked+" "+this.pageX+" "+this.pageY+" "+this.dimX+" "+this.dimY;
+	// alert(rtn);
+	return clicked;
 }
--- javascripts/device_maker.js
+++ javascripts/device_maker.js
@@ -6,17 +6,27 @@ var canvasWidth = 1000;
 var canvasHeight = 800;
 var backgroundColor = "#333333";
 var gridColor = "#ffffff";
-var c;
 
 var layoutScale = 1;
+var gridSize = 20*layoutScale;
+var layoutBoundaryX = 800;
+var layoutBoundaryY = 800;
+
+var c;
+var componentFixedColor = "rgba(255, 255, 255, 0.8)";
+var componentSelectedColor = "rgba(0, 255, 0, 0.45)";
 var componentFontSize = 20*layoutScale;
 
+
+
+var mouseState = 0; //0:nothing, 1:clicked
+var mouseX, mouseY;
+var gridX, gridY;
 
 /*
 	Initialize the canvas, drawing the grid of layout.
 */
-function initCanvas()
-{
+function initCanvas() {
 	// Create a canvas
 	var canvasDiv = document.getElementById('canvasDiv');
 	canvas = document.createElement('canvas');
@@ -32,7 +42,7 @@ function initCanvas()
 	context.fillStyle = backgroundColor;
     context.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    c = new Component("BLE", 300, 100, 100, 100);
+    c = new Component("BLE", 200, 100, 100, 100);
 
     redraw();
 
@@ -40,8 +50,7 @@ function initCanvas()
 
 
 // Redraw current state.
-function redraw()
-{
+function redraw() {
 	context.clearRect(0, 0, canvasWidth, canvasHeight);
 	drawGrid();
 	drawComponent(c);
@@ -50,11 +59,10 @@ function redraw()
 
 
 // Draw background grid.
-function drawGrid()
-{
+function drawGrid() {
 	context.beginPath();
 	context.lineWidth = 0.1;
-    for (var i = 0; i <= canvasHeight; i=i+20*layoutScale) {
+    for (var i = 0; i <= canvasHeight; i=i+gridSize) {
     	context.moveTo(i, 0);
     	context.lineTo(i, canvasHeight);
     	context.moveTo(0, i);
@@ -62,30 +70,95 @@ function drawGrid()
     }
     context.closePath();
 
-	context.strokeStyle = gridColor;
+	context.strokeStyle = gridColor; 
 	context.stroke();
 }
 
-function drawComponent(component)
-{
+function drawComponent(component) {
   	context.beginPath();
 	context.rect(component.pageX*layoutScale, component.pageY*layoutScale, 
 		component.dimX*layoutScale, component.dimY*layoutScale);
-	context.fillStyle = 'white';
+	context.fillStyle = component.color;
 	context.fill();
 	context.closePath();
 	context.stroke();
 
 	context.font = componentFontSize+"pt Calibri";
-  	context.fillStyle = 'gray';
+  	context.fillStyle = "rgba(50, 50, 50, 1)";
   	context.fillText(component.name, component.labelX()*layoutScale, component.labelY()*layoutScale);
 }
 
+var mouseEvent = function(e) {
+	mouseX = e.clientX-canvas.offsetLeft;
+    mouseY = e.clientY-canvas.offsetTop;
+    gridX = Math.round(mouseX/gridSize)*gridSize/layoutScale;
+    gridY = Math.round(mouseY/gridSize)*gridSize/layoutScale;
+    boundaryX = layoutBoundaryX-(c.dimX*layoutScale);
+    boundaryY = layoutBoundaryY-(c.dimY*layoutScale);
+    if(mouseX>= boundaryX) {
+    	c.pageX = boundaryX/layoutScale;
+    }
+    else {
+       	c.pageX = gridX;
+    }
+    if(mouseY>= boundaryY) {
+    	c.pageY = boundaryY/layoutScale;
+	}
+    else {
+		c.pageY = gridY;
+	}
+}
+
 function onClick(e) {
+
+	switch(mouseState) {
+    case 0:
+    	// alert(e.pageX-canvas.offsetLeft+" "+e.pageY-canvas.offsetTop);
+        if(c.isComponentClicked(e.pageX-canvas.offsetLeft, e.pageY-canvas.offsetTop, layoutScale)) {
+			canvas.addEventListener('mousemove', mouseEvent, false);
+		    mouseState = 1;
+		    c.color = componentSelectedColor;
+			requestAnimFrame(dragComponent);
+		}
+        break;
+    case 1:
+    	mouseState = 0;
+        break;
+    default:
+        
+	}
 	
 	redraw();
-    context.font = '20pt Calibri';
+    context.font = '16pt Calibri';
   	context.fillStyle = 'gray';
-  	context.fillText(e.pageX+"  "+e.pageY, 850, 300);
+  	context.fillText((e.pageX-canvas.offsetLeft)+"  "+(e.pageY-canvas.offsetTop)+" "+mouseState+" "+c.pageX+" "+c.pageY, 800, 300);
+  	context.fillText(c.isComponentClicked(e.pageX-canvas.offsetLeft, e.pageY-canvas.offsetTop, layoutScale)+" "+c.boundaryX+" "+c.boundaryY, 800, 350);
 }
+
+function dragComponent() {
+
+	if(mouseState == 0) { // Clicked for placing component on layout.
+		canvas.removeEventListener('mousemove', mouseEvent);
+		c.color = componentFixedColor;
+		c.boundaryX = c.pageX + c.dimX;
+		c.boundaryY = c.pageY + c.dimY;
+		redraw();
+		return;
+	}
+	
+	redraw();
+
+	requestAnimFrame(dragComponent);
+	context.font = '16pt Calibri';
+  	context.fillStyle = 'gray';
+  	context.fillText(mouseX+" "+mouseY+" "+Math.round(mouseX/gridSize)*gridSize+" "+Math.round(mouseX/gridSize)*gridSize, 800, 400);
+}
+
+window.requestAnimFrame = (function(callback) {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame 
+	|| window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+	function(callback) {
+	  window.setTimeout(callback, 1000 / 60);
+	};
+})();
 
